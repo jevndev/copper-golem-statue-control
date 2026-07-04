5724b625d12a421a7fd21309292914140791880e
jevndev <jevndev@jevn.dev>
2026-07-04 12:05:58 -0500

    Get MVP

--- src/main/java/dev/jevn/copper_golem_statue_control/CopperGolemStatueControl.java
+++ src/main/java/dev/jevn/copper_golem_statue_control/CopperGolemStatueControl.java
@@ -1,4 +1,4 @@
-package jevndev.copper_golem_statue_control;
+package dev.jevn.copper_golem_statue_control;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -9,19 +9,11 @@ import org.slf4j.LoggerFactory;
 
 public class CopperGolemStatueControl implements ModInitializer {
 	public static final String MOD_ID = "copper-golem-statue-control";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initializing " + MOD_ID);
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/dev/jevn/copper_golem_statue_control/mixin/CopperGolemStatueBlockMixin.java
@@ -0,0 +1,63 @@
+package dev.jevn.copper_golem_statue_control.mixin;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+
+import dev.jevn.copper_golem_statue_control.CopperGolemStatueControl;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CopperGolemStatueBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.redstone.Orientation;
+
+@Mixin(CopperGolemStatueBlock.class)
+public class CopperGolemStatueBlockMixin extends Block {
+    public CopperGolemStatueBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected void neighborChanged(
+            final BlockState state,
+            final Level level,
+            final BlockPos pos,
+            final Block block,
+            final @Nullable Orientation orientation,
+            final boolean movedByPiston
+    ) {
+        CopperGolemStatueControl.LOGGER.info("Neighbor block changed at position: " + pos);
+        if (level instanceof ServerLevel serverLevel) {
+            this.updatePose(state, serverLevel, pos);
+        }
+    }
+
+
+    private void updatePose(BlockState state, ServerLevel level, BlockPos pos) {
+        if (level.hasNeighborSignal(pos))
+        {
+            int powerLevel = level.getBestNeighborSignal(pos);
+
+            CopperGolemStatueBlock.Pose currentPose = state.getValue(CopperGolemStatueBlock.POSE);
+            CopperGolemStatueBlock.Pose pose = this.getPoseFromPowerLevel(powerLevel);
+
+            if (pose != null && pose != currentPose) {
+                level.playSound((Entity)null, pos, SoundEvents.COPPER_GOLEM_BECOME_STATUE, SoundSource.BLOCKS);
+                level.setBlock(pos, state.setValue(CopperGolemStatueBlock.POSE, pose), 3);
+            }
+        }
+    }
+
+    private @Nullable CopperGolemStatueBlock.Pose getPoseFromPowerLevel(int powerLevel) {
+        if (powerLevel == 0 || powerLevel > CopperGolemStatueBlock.Pose.values().length) {
+            return null;
+        }
+        return CopperGolemStatueBlock.Pose.values()[powerLevel - 1];
+    }
+
+
+}
